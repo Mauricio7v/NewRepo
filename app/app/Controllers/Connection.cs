d44585c6d409801ab9db130ab11f1ed2082c94e1
José Mauricio Medrano Valle <mauriciov1902@hotmail.com>
2018-10-04 14:15:42 -0600

    Clase Connection

---
+++ app/app/Controllers/Connection.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using app.Models;
+using System.Data.Entity;
+
+namespace app.Controllers
+{
+    public class Connection : DbContext
+    {
+        public DbSet<Usuario> ObjRegisterUser { get; set; }
+    }
+}
