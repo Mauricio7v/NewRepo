6e5425b707887be93e4404b24fc90ffe277ed4d6
José Mauricio Medrano Valle <mauriciov1902@hotmail.com>
2018-10-04 15:11:31 -0600

    .

--- app/app/App_Start/RouteConfig.cs
+++ app/app/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace app
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Login", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "SignUp", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
